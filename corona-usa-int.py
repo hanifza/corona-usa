196def7a779f79ca0086ef021a0148ec390e3c1a
hanifza <63756363+hanifza@users.noreply.github.com>
2020-04-15 23:33:25 -0400

    Update corona-usa-int.py

--- corona-usa-int.py
+++ corona-usa-int.py
@@ -1,20 +1,37 @@
 from time import sleep
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
+import os
+
+now = datetime.now()
+#iterations
+i = 1
 
 #total live number fetcher (web scraper)
 def update():
   page = requests.get('https://www.worldometers.info/coronavirus/country/us/')
-  soup = BeautifulSoup(page.content, 'html.parser') #gathers HTML link
-  cases = str(list(soup.find_all('span')[4])) #HTML line w/ total
+  soup = BeautifulSoup(page.content, 'html.parser')
+  cases = str(list(soup.find_all('span')[4]))
   cases = cases.replace('[\'', '')
   cases = cases.replace(',', '')
   cases = cases.replace(' \']', '')
 
   return int(cases)
 
+def printing(total):
+  #Clears the screen
+  os.system('cls' if os.name == 'nt' else 'clear')
+  print(f'''
+    Date: {now.month}/{now.day}/{now.year}
+    Total: {total}
+    Iteration: {i}''')
 
-#repeating update
+#initial print
+printing(update())
+
+#repeating print
 while True:
-  update()
-  sleep(60)
+  printing(update())
+  i += 1
+  sleep(2)
